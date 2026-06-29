50afd992884166e17e54c26ed9a91ad62393aa3b
Dmitriy Kolesnik <d.kolisnyk@protonmail.com>
2026-06-29 15:53:44 +0400

    chore: add local database infrastructure and Prisma foundation

---
+++ apps/api/prisma/prisma.service.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error('DATABASE_URL is required to initialize PrismaClient');
+}
+
+const adapter = new PrismaPg({
+  connectionString: databaseUrl,
+});
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
